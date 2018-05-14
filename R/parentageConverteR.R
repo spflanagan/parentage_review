09b87f99f3d533957ec6ffd847355a9144c6271c
spflanagan <sflanagan19@gmail.com>
2018-05-14 08:59:39 -0400

    running programs

--- R/parentageConverteR.R
+++ R/parentageConverteR.R
@@ -44,3 +44,44 @@ generate_seq_lifehist<-function(GenoM.ID,sexes=c(Males="MAL",Females="FEM",Offsp
   LifeHistDat$BY[grep(sexes["Offspring"],LifeHistDat$ID)]<-2
   return(LifeHistDat)
 }
+
+
+#' Convert a CERVUS file into input for FaMoz
+#' @param gty A data.frame of genotypes in CERVUS format
+#' @param filename The name of the file to export the FaMoz file to.
+#' @param first.allele The first column with an allele (locus 1, alele 1). Default is 4.
+#' @param miss The character specifying missing data. Default is 0.  
+#' @param sexes A labeled list with Males, Females, and Offspring elements all designating strings found in ID names that identify individuals as males, females and offspring
+#' @export
+cervus2famoz<-function(gty,filename,first.allele=4,miss=0,sexes=c(Males="MAL",Females="FEM",Offspring="OFF")){
+  nloci<-(ncol(gty)-(first.allele-1))/2
+  #write loci
+  suppressWarnings(write.table(nloci,filename,quote=FALSE,col.names = FALSE,row.names = FALSE))
+  frqs<-lapply(seq(first.allele,ncol(gty),2),function(loc1,gty){
+    locus<-gty[,c(loc1,loc1+1)]
+    frq<-unlist(locus[locus!=miss])
+    dat<-data.frame(t(data.frame(table(frq)/sum(table(frq)))$Freq))
+    row.names(dat)<-ncol(dat)
+    suppressWarnings(write.table(dat,filename,append = TRUE,row.names=TRUE,col.names = FALSE,quote=FALSE))
+  },gty=gty)
+  #how many parents?
+  ns<-c(length(c(grep(sexes["Males"],gty$ID),grep(sexes["Females"],gty$ID))),
+        length(grep(sexes["Offspring"],gty$ID)))
+  suppressWarnings(write.table(t(ns),filename,append = TRUE,row.names=FALSE,col.names = FALSE,quote=FALSE))
+  suppressWarnings(write.table('',filename,append = TRUE,row.names=FALSE,col.names = FALSE,quote=FALSE))
+  #parent genotypes
+  gty[gty==miss]<--5
+  parents<-as.matrix(gty[c(grep(sexes["Males"],gty$ID),grep(sexes["Females"],gty$ID)),
+                         first.allele:ncol(gty)])
+  rownames(parents)<-NULL
+  suppressWarnings(write.table(parents,filename,append = TRUE,row.names=TRUE,col.names = FALSE,quote=FALSE))
+  suppressWarnings(write.table('',filename,append = TRUE,row.names=FALSE,col.names = FALSE,quote=FALSE))
+  #offspring genotypes
+  offspring<-as.matrix(gty[grep(sexes["Offspring"],gty$ID),first.allele:ncol(gty)])
+  rownames(offspring)<-NULL
+  suppressWarnings(write.table(offspring,filename,append = TRUE,row.names=TRUE,col.names = FALSE,quote=FALSE))
+  
+}
+
+
+
